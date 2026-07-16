55369cb1dc2a64a08f776266f4b8cb9b3916648b
maycon-douglasd <maycon_douglasds@hotmail.com>
2026-07-16 17:22:07 -0300

    feat: create responsive navbar component

---
+++ src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar__container">
+                <h2 className="navbar__logo">ASCEND</h2>
+
+                <ul className="navbar__menu">
+                    <li><a href="#">Início</a></li>
+
+                    <li><a href="#">Sobre</a></li>
+
+                    <li><a href="#">Serviços</a></li>
+
+                    <li><a href="#">Contato</a></li>
+                </ul>
+
+                <button className="navbar__button">Vamos Conversar
+                </button>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <main>
+      <h1>Project ASCEND</h1>
+      <p>Landing Page em desenvolvimento 🚀</p>
+    </main>
+  );
+}
+
+export default Home;
